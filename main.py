88803d2b26fec01a07c329afa1684a58ee6fd26e
Safari Germain <pzx115866@student.poznan.merito.pl>
2024-01-02 15:23:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+ASP.NET: only supported by windows
+why ASP.NET CORE
+-> cross platform development
+-> Open source- 
+build web apps and services, IoT and mobile backends
+deploy on cloud computing
+
+server rendered UI
+app UI is dynamically rendered on the server
+
+
+"""
