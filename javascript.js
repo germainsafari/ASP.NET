e10522b0032a3ab13d5e829cadd10f612721b355
Safari Germain <pzx115866@student.poznan.merito.pl>
2024-01-04 13:29:10 +0100

    messag

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 const myFuntion = () => {
-    
+
 }
+/*
+Placing scripts in external files has some advantages:
+
+It separates HTML and code
+It makes HTML and JavaScript easier to read and maintain
+Cached JavaScript files can speed up page loads
+To add several script files to one page  - use several script tags: */
