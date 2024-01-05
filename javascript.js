e143012d1c674766751dc3c828752aed1534fe22
Safari Germain <pzx115866@student.poznan.merito.pl>
2024-01-05 16:11:07 +0100

    final

--- javascript.js
+++ javascript.js
@@ -8,3 +8,14 @@ It separates HTML and code
 It makes HTML and JavaScript easier to read and maintain
 Cached JavaScript files can speed up page loads
 To add several script files to one page  - use several script tags: */
+
+const object = {
+    surname: 'Safari',
+    firstname: 'Trench',
+    age: 12
+};
+
+all = ""
+for (let i in object) {
+    all += object[i] + ""
+};
